4c5fea8f23b3164d28ab10f7fdf89c23c42ca09f
numba19 <zachlegesse@gmail.com>
2024-02-19 14:07:20 -0500

    added a button that gets a random question from the API added a form to check answer validity (broken)

--- src/App.js
+++ src/App.js
@@ -3,25 +3,30 @@ import './App.css';
 import React, {useState} from 'react';
 
 
-const apiurl = "http://localhost:3000/questions/random"
+const apiurl = "http://127.0.0.1:3000/questions/random"
+const checkQuestion = "http://127.0.0.1:3000/questions/submit"
 
 
 
 
 function App() {
-  const [content, setContent] = useState("test")
+  const [content, setContent] = useState('')
+  
+  if(content === ''){
+    getQuestion()
+  }
 
-  function callApi () {
+  function getQuestion () {
 
     fetch(apiurl)
     .then(response => {
       if(!response.ok) {
         throw new Error("Network response was not ok")
       }
-      content = String(response.text());
+      response.json().then( json => {setContent(json.message)})
     }) 
-    .then(data => {
-      setContent(data);
+    .then(data => { 
+      console.log(String(data))
     })
     .catch(error => {
       console.error("Error:", error);
@@ -30,12 +35,24 @@ function App() {
   
   }
 
-  callApi();
+  if( content == null){
+    getQuestion()
+  }
 
   return (
     <div className="App">
-
-        <h1>{content}</h1>
+        
+        <button onClick={getQuestion}>get question</button>
+        
+        <div>
+          <h1>{content} = ?</h1>
+          <form action={checkQuestion} target='_blank'>
+            <input type="hidden" value={content}></input>
+            <input type="text" id='question'></input>
+            <input type="submit" id='answer'></input>
+          </form>
+        </div>
+        
 
     </div>
   );
