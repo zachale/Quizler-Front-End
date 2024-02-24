2bfb77a3f1e10f90427bb943448142698dedff1b
numba19 <zachlegesse@gmail.com>
2024-02-24 13:59:04 -0500

    fixed answer submission delay added correct/incorrect message added timer (broken)

--- src/App.js
+++ src/App.js
@@ -1,52 +1,81 @@
 
 import './App.css';
-import React, {useState} from 'react';
+import React, {useState, useEffect} from 'react';
 
 
 const getQuestion = "http://127.0.0.1:3000/questions/random"
 const checkQuestion = "http://127.0.0.1:3000/questions/submit"
 
-function SubmitAnswerFrom({question, onSubmit}){
-  
-  return (
-    <>
-      <form onSubmit={onSubmit} method="GET">
-        <input type="hidden"  id='question' name='question' value={question}></input>
-        <input type="text" id='answer' name='answer'></input>
-        <input type="submit"></input>
-      </form>
-    </>
-  )
-}
-
 async function getJsonResponse(url) {
-
-
+  //Calls the API at the given URL and returns the response
   try {
     const response = await fetch(url);
-
     if(!response.ok) {
       throw new Error("Network response was not ok")
     }
-
     const value = await response.json()
-    return value
-    
-    
+    return value   
   } catch (error) {
     console.error("Error:", error);
   } 
 
 }
+function CountDown(){
+  const [width,setWidth] = useState(100)
+  const [fill,setFill] = useState("rgb(150,0,10")
+  const [intervalID, setIntervalId] = useState(0)
+
+  useEffect( () => setIntervalId(setInterval(updateCountDown, 1000)), [])
+
+  function updateCountDown(){
+    console.log(width)
+    setWidth(width.valueOf() - 1)
+  }
+
+  if (width < 0){
+    clearInterval(intervalID)
+  }
+  
+  return (
+   <svg width="100%" height="70" xmlns="http://www.w3.org/2000/svg">
+    <rect className='timer' width={String(width) + '%'} height="100%" x="0" y="0" rx="0" ry="0" fill={fill} />
+  </svg>)
+}
+
+function SubmitAnswerFrom({question, onSubmit}){
+
+  const [answer, setAnswer] = useState('') 
+  
+  return (
+    <>
+      <form onSubmit={handleSubmit} method="GET">
+        <input type="hidden"  id='question' name='question' value={question}></input>
+        <input type="text" id='answer' name='answer' value={answer} onChange={(e) => setAnswer(e.target.value)}></input>
+        <input type="submit"></input>
+      </form>
+    </>
+  )
+
+  function handleSubmit(e){
+    e.preventDefault()
+    onSubmit(e)
+    setAnswer('')
+  }
+}
+
 
 
 function App() {
   const [question, setQuestion] = useState('')
   const [validAnswer, setValidAnswer] = useState('')
   const [score, setScore] = useState(0)
+  const [notif, setNotif] = useState('')
 
   async function updateQuestion(e){
-    await getJsonResponse(getQuestion).then( promise => {setQuestion(promise.message)})
+    await getJsonResponse(getQuestion)
+      .then( promise => {
+        setQuestion(promise.message)
+      })
   }
 
   async function updateAnswer(e){
@@ -56,19 +85,21 @@ function App() {
     const url = checkQuestion + '?question=' + query + '&answer=' + e.target.answer.value 
     // This is so primitive
     // This is definitely not the optimal way to do this
-    // I couldn't find the clean way
+    // There are definitely security risks here
 
     //submit the question and process the result
     await getJsonResponse(url)
-      .then( promise => {setValidAnswer(value => value = promise.message, handleAnswerCheck(promise.message))})
+      .then( promise => handleAnswerCheck(promise.message))
   }
 
   function handleAnswerCheck(e = null){
     setValidAnswer(e)
-    if(validAnswer){
+    if(Boolean(e)){
       setScore(score.valueOf() + 1)
       updateQuestion()
-      console.log("it was true!")
+      setNotif("Correct!")
+    } else {
+      setNotif("Incorrect, try again!")
     }
     console.log("answer should be " + e + " " + validAnswer)
   }
@@ -80,9 +111,11 @@ function App() {
   return (
     <div className="App">      
         <div>
+          <CountDown/>
           <h1>{question} = ?</h1>
           <SubmitAnswerFrom question={question} onSubmit={updateAnswer}/>
           <h1>{String(score)}</h1>
+          <p>{notif}</p>
         </div> 
     </div>
   );
